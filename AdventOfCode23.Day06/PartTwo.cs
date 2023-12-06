fad1814189e9dac10df35cdf7177a64972836bad
Nicklas Scherm <nicklas.scherm@stud.h-da.de>
2023-12-06 06:41:21 +0100

    Add solution for day 06 part 2

--- AdventOfCode23.Day06/PartTwo.cs
+++ AdventOfCode23.Day06/PartTwo.cs
@@ -1,11 +1,44 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode23.Day06;
 
 static class SolutionDay06Part02
 {
+    record Race(long Duration, long RecordDistance);
     public static void Solution()
     {
+        var lines = File.ReadAllLines("input06.txt").ToList();
+        var race = Parse(lines);
+        var waysToWin = GetNumberOfWaysToWin(race);
+        
+        Console.WriteLine(waysToWin);
+    }
 
+    static Race Parse(List<string> lines)
+    {
+        var durationString = lines[0].Replace(" ", "");
+        var distanceString = lines[1].Replace(" ", "");
+        var pattern = @"\d+";
+        var regex = new Regex(pattern);
+        var duration = long.Parse(regex.Match(durationString).Value);
+        var distance = long.Parse(regex.Match(distanceString).Value);
 
-        Console.WriteLine("");
+        return new Race(duration, distance);
+    }
+
+    static int GetNumberOfWaysToWin(Race race)
+    {
+        var waysToWin = 0;
+        for (long i = 0; i <= race.Duration; i++)
+        {
+            var chargeTime = i;
+            var moveTime = race.Duration - i;
+            if (chargeTime * moveTime > race.RecordDistance)
+            {
+                waysToWin++;
+            }
+        }
+
+        return waysToWin;
     }
 }
--- AdventOfCode23.Day06/Program.cs
+++ AdventOfCode23.Day06/Program.cs
@@ -5,9 +5,9 @@ class SolutionDay06
     static void Main(string[] args)
     {
         // Part One
-        SolutionDay06Part01.Solution();
+        //SolutionDay06Part01.Solution();
 
         // Part Two
-        //SolutionDay06Part02.Solution();
+        SolutionDay06Part02.Solution();
     }
 }
