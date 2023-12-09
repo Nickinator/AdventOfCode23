b5736e7e6ca9df7183a32ff4a1221f04d46bba80
Nicklas Scherm <nicklas.scherm@stud.h-da.de>
2023-12-09 09:21:42 +0100

    Add solution for day 09 part 1

--- AdventOfCode23.Day09/PartOne.cs
+++ AdventOfCode23.Day09/PartOne.cs
@@ -1,11 +1,57 @@
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode23.Day09;
 
 static class SolutionDay09Part01
 {
     public static void Solution()
     {
+        var lines = File.ReadAllLines("input09.txt").ToList();
+        var report = Parse(lines);
 
+        var sumOfNextValues = report
+            .Select(d => ExtrapolateSequence(d))
+            .Sum();
 
-        Console.WriteLine("");
+        Console.WriteLine(sumOfNextValues);
+    }
+
+    static int ExtrapolateSequence(List<int> dataset)
+    {
+        if (dataset.All(n => n is 0))
+        {
+            return 0;
+        }
+
+        var differences = new List<int>();
+        for (int i = 0; i < dataset.Count - 1; i++)
+        {
+            var diff = dataset[i + 1] - dataset[i];
+            differences.Add(diff);
+        }
+
+        dataset.Add(dataset.Last() + ExtrapolateSequence(differences));
+        return dataset.Last();
+    }
+
+    static List<List<int>> Parse(List<string> lines)
+    {
+        var report = new List<List<int>>();
+        foreach (var line in lines)
+        {
+            report.Add(ParseDataset(line));
+        }
+        return report;
+    }
+
+    static List<int> ParseDataset(string line)
+    {
+        var pattern = @"-?\d+";
+        var regex = new Regex(pattern);
+        var matches = regex.Matches(line);
+        return matches
+            .Select(m => int.Parse(m.Value))
+            .ToList();
     }
 }
