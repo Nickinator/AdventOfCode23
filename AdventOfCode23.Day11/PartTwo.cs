500f5e2614057ddf41c985e0fc7c0b705e1dfa08
Nicklas Scherm <nicklas.scherm@stud.h-da.de>
2023-12-11 10:03:43 +0100

    Add project for day 11

---
+++ AdventOfCode23.Day11/PartTwo.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode23.Day11;
+
+static class SolutionDay11Part02
+{
+    public static void Solution()
+    {
+
+
+        Console.WriteLine("");
+    }
+}
---
+++ AdventOfCode23.Day11/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode23.Day11;
+
+class SolutionDay11
+{
+    static void Main(string[] args)
+    {
+        // Part One
+        SolutionDay11Part01.Solution();
+
+        // Part Two
+        SolutionDay11Part02.Solution();
+    }
+}
