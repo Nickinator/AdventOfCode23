074792b925d2ca369cabef5c0485f23fb1490afd
Nicklas Scherm <nicklas.scherm@stud.h-da.de>
2023-12-15 07:20:20 +0100

    Add solution for day 15 part 1

--- AdventOfCode23.Day15/PartOne.cs
+++ AdventOfCode23.Day15/PartOne.cs
@@ -4,8 +4,26 @@ static class SolutionDay15Part01
 {
     public static void Solution()
     {
+        var input = File.ReadAllText("input15.txt");
+        var steps = input.Split(',');
 
+        var sumOfHashes = steps
+            .Select(s => Hash(s))
+            .Sum();
+        Console.WriteLine(sumOfHashes);
+    }
 
-        Console.WriteLine();
+    private static int Hash(string word)
+    {
+        var currentValue = 0;
+        foreach (char c in word)
+        {
+            int ascii = (int)c;
+            currentValue += ascii;
+            currentValue *= 17;
+            currentValue %= 256;
+        }
+
+        return currentValue;
     }
 }
